1db83c55adba8d1b8afcc759f00f37f20186f078
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2021-08-09 11:50:34 +0530

    user profile

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../Assets/logo'
 import './Navbar.css'
+import axios from 'axios';
+import Logout from '../Assets/logout'
 const Navbar = (props) => {
 
     const [ showPhoneMenu , setPhoneMenu ] = useState(false)
+    const [ name , setName ] = useState()
+    const [ email , setEmail ] = useState()
+
+    useEffect( () => {
+        const token = localStorage.getItem('token')
+
+        if ( token && token.length >= 4){
+            axios.post('http://artwindow.herokuapp.com/art/verifyUser', {
+                token
+            })
+            .then ( res => {
+                if( res.data === 'failure'){
+                    // window.location.assign('/')
+                    console.log('failed')
+                }
+                else{
+                    axios.post('http://artwindow.herokuapp.com/art/getUser', {id :res.data})
+                    .then ( resp => {
+                        console.log(resp.data)
+                        setName(resp.data.name)
+                        setEmail(resp.data.email)
+                    })
+            
+                    .catch( err => {
+                        console.log(err)
+                    })
+            
+                }
+            })
+      
+            .catch( err => {
+                console.log(err)
+            })
+        }
+     
+    } , [])
+
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        window.location.assign('/')
+    }
     return ( 
         <div className="Navbar">
             <div className="navbar-desktop">
@@ -16,6 +59,13 @@ const Navbar = (props) => {
                     <Link to="/bout" style={{textDecoration:'none'}}>  <div className={ props.active === "about" ? "active-navbar-list" : "navbar-list" }>About us</div> </Link>
                   <Link to="/reach" style={{textDecoration:'none'}}>  <div className={ props.active === "contact" ? "active-navbar-list" : "navbar-list" }>Contact us</div> </Link>
                   <Link to="/projects" style={{textDecoration:'none'}}> <div className={ props.active === "projects" ? "active-navbar-list" : "navbar-list" }>Cart</div> </Link>
+                  <div className="logout">
+                      <div className="inside-log">
+                      <div className="logout-svg" onClick={handleLogout}> <Logout></Logout></div>
+                      <div className="profile-name">{name}</div>
+                      <div className="profile-email">{email}</div>
+                      </div>
+                  </div>
                 </div>
             </div>
 
@@ -37,11 +87,20 @@ const Navbar = (props) => {
                        <Link to="/bout" style={{textDecoration:'none'}}>   <div className="hamburger-phone-list">About us</div> </Link>
                        <Link to="/reach" style={{textDecoration:'none'}}>    <div className="hamburger-phone-list">Contact us</div>  </Link>
                        <Link to="/projects" style={{textDecoration:'none'}}>   <div className="hamburger-phone-list">Cart</div>    </Link>
+                       <div className="phone-profile">Logged in as <br></br> {name} &nbsp; | &nbsp; {email}</div>
+                       <div className="phone-log"> 
+                        <div className="phone-logout" onClick={handleLogout}> 
+                        <Logout></Logout> 
+                        </div>
+                        <div className="log-text">Logout </div>
+                    </div>
                            
                         </div> : null }
 
                 
                         </div> 
+
+                   
                 
         </div>
      );
