ddd3f063cbf252a51c5e99813240b072879c2bbd
Talha-shamim <Talhashamim001@gmail.com>
2021-10-31 18:17:39 +0530

    mail

--- src/Components/bookingConfirm/bookingConfirm.js
+++ src/Components/bookingConfirm/bookingConfirm.js
@@ -21,17 +21,12 @@ const BookingConfirm = () => {
     const [ productsConfirm , setProductsConfirm ] = useState()
 
     const cartItems = localStorage.getItem('cartItems')
-    const cartArray = cartItems.split(",")
+    let cartArray = cartItems.split(",")
+    
 
     useEffect( () => {
-        axios.post('http://localhost:3500/art/getArtById' ,cartArray)
-        .then( res => {
-            setProductsConfirm(res.data)
-        })
+        
 
-        .catch( err => {
-            console.log('errr in cartpage')
-        })
     } , [] )
 
     console.log(productsConfirm)
@@ -49,7 +44,6 @@ const BookingConfirm = () => {
         if(e.target.name === 'phoneNumber'){
             setPhoneNumber(e.target.value)
         }
-
     }
 
       
@@ -84,15 +78,38 @@ const BookingConfirm = () => {
 
 
     const handleSubmit = (e) =>{
+        cartArray = cartArray.filter(item => item !== 'undefined'   )
+
+        const data1 = {
+            name_ : name,
+            email_ : email,
+            add : address,
+            ph : phoneNumber,
+            item : cartArray
+        }
+
         const data = {
             name,
             email,
             address,
             phoneNumber
         }
+        
         const error= validate(data)
+
+
         if(error){
             toast(`Please Check Your Data Once Again`)
+        }
+        else{
+            axios.post('http://localhost:3500/art/mail' ,data1)
+            .then( res => {
+                setProductsConfirm(res.data)
+            })
+
+            .catch( err => {
+                console.log('errr in cartpage')
+            })
         }
         setResult(true)
     }
